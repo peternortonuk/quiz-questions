30cebff2b5ea8eb42088d544b31e7cc87415a4d0
Peter Norton <peter.norton@gazprom-mt.com>
2021-01-10 22:01:18 +0000

    decorators 2

---
+++ hackerrank/decorators_2_name_directory.py
@@ -0,0 +1,64 @@
+"""
+https://www.hackerrank.com/challenges/decorators-2-name-directory/problem
+
+Let's use decorators to build a name directory! You are given some information about  people. Each person has a first
+name, last name, age and sex. Print their names in a specific format sorted by their age in ascending order i.e. the
+youngest person's name should be printed first. For two people of the same age, print them in the order of their input.
+
+For Henry Davids, the output should be:
+
+Mr. Henry Davids
+For Mary George, the output should be:
+
+Ms. Mary George
+Input Format
+
+The first line contains the integer , the number of people.
+ lines follow each containing the space separated values of the first name, last name, age and sex, respectively.
+
+Constraints
+
+
+Output Format
+
+Output  names on separate lines in the format described above in ascending order of age.
+
+Sample Input
+
+3
+Mike Thomson 20 M
+Robert Bustle 32 M
+Andria Bustle 30 F
+Sample Output
+
+Mr. Mike Thomson
+Ms. Andria Bustle
+Mr. Robert Bustle
+Concept
+
+For sorting a nested list based on some parameter, you can use the itemgetter library. You can read more about it here.
+"""
+import operator
+from tests.test_decorators_2_name_directory import input, expected
+
+
+def person_lister(f):
+    def inner(people):
+        people = sorted(people, key=lambda x: int(x[2]))
+        for person in people:
+            yield f(person)
+    return inner
+
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+
+if __name__ == '__main__':
+
+    people = input
+    people = [person.split() for person in people]
+    output = name_format(people)
+
+    assert list(output) == expected
